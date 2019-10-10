1d90d787f68781ffaa898a9b54abd5652190054a
Erin Polley <erinepolley@gmail.com>
2019-10-10 14:51:24 -0500

    completed chicken monkey exercise!

---
+++ src/scripts.js
@@ -0,0 +1,9 @@
+for (let num = 1; num < 101; num++) {
+    if (num % 5 == 0) {
+        console.log (`${num} Chicken`)
+    } else if (num % 7 == 0) {
+        console.log (`${num} Monkey`)
+    } else {
+        console.log (num)
+    }
+};
