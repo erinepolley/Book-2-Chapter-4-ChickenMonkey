ae084e65b166e7306fc281d1973e5ad57b9b4328
Erin Polley <erinepolley@gmail.com>
2019-10-18 15:28:04 -0500

    added chicken monkey

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,7 @@
 for (let num = 1; num < 101; num++) {
-    if (num % 5 == 0) {
+    if(num % 5 ==0 && num % 7 == 0) {
+    console.log(`${num} Chicken Monkey`)
+    } else if (num % 5 == 0) {
         console.log (`${num} Chicken`)
     } else if (num % 7 == 0) {
         console.log (`${num} Monkey`)
